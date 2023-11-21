29008e57c6adbe0a63548eb479e9ebfce03ac6e6
Randall Vargas <ranvlicr@gmail.com>
2023-11-20 19:11:13 -0600

    Updated program.cs to register db context and services & repositories

--- Backend/ClothingPartnerAPI/ClothingPartnerAPI/Program.cs
+++ Backend/ClothingPartnerAPI/ClothingPartnerAPI/Program.cs
@@ -1,3 +1,9 @@
+using ClothingPartnerAPI.Services.Contracts;
+using ClothingPartnerAPI.Services;
+using ClothingPartnerAPI.DAL.Contracts;
+using ClothingPartnerAPI.DAL;
+using ClothingPartnerAPI.DAL.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//register db context
+builder.Services.AddDbContext<ClothingPartnerContext>();
+
+//register services & repositories
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
 
 var app = builder.Build();
 
